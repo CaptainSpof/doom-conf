508b978fcc101ccf819e130508eaeb0dbdd1ee10
CaptainSpof <captain.spof@gmail.com>
2021-01-21 20:30:11 +0100

    add a bunch of theme packages
    add nov.el

--- packages.el
+++ packages.el
@@ -22,17 +22,25 @@
 (package! verb)
 (package! company-tabnine) ; tab9 autocomplete
 (package! groovy-mode)
-(package! systemd :pin "51c148e09a")
+;; (package! systemd :pin "51c148e09a")
 (package! xterm-color :pin "d53a39a5af72cd340ebf686e59a37289b4cb6e8c")
 (package! magit-delta
   :recipe (:host github :repo "dandavison/magit-delta" :files ("magit-delta.el")))
 (package! exec-path-from-shell)
 (package! git-messenger)
 (package! company-fuzzy)
-(package! org-super-agenda :pin "3264255989021b8563ea42b5d26acbc2a024f14d")
+(package! org-super-agenda)
 (package! org-pretty-table-mode
   :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "474ad84a8fe5377d67ab7e491e8e68dac6e37a11")
 (package! org-pretty-tags :pin "40fd72f3e701e31813f383fb429d30bb88cee769")
 ;; (package! burly)
 ;; (package! sublimity)
 (package! direnv)
+(package! kaolin-themes)
+(package! color-theme-sanityinc-tomorrow)
+(package! apropospriate-theme)
+(package! solarized-theme)
+;; (package! nano-emacs
+;;   :recipe (:host github :repo "rougier/nano-emacs"))
+(package! nix-mode)
+(package! nov)
