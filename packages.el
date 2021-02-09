df26cd18a33d0c54f11ab0fbdffbacf2baba30c9
CaptainSpof <captain.spof@gmail.com>
2021-02-09 12:15:51 +0100

    cleanup packages.el

--- packages.el
+++ packages.el
@@ -7,31 +7,32 @@
 ;; (package! builtin-package :disable t)
 
 (package! verb)
-;; (package! company-tabnine) ; tab9 autocomplete
 (package! groovy-mode)
-;; (package! systemd :pin "51c148e09a")
 (package! xterm-color :pin "d53a39a5af72cd340ebf686e59a37289b4cb6e8c")
 (package! magit-delta
   :recipe (:host github :repo "dandavison/magit-delta" :files ("magit-delta.el")))
 (package! exec-path-from-shell)
 (package! git-messenger)
 (package! company-fuzzy)
-;; (package! org-super-agenda)
 (package! org-pretty-table-mode
   :recipe (:host github :repo "Fuco1/org-pretty-table") :pin "474ad84a8fe5377d67ab7e491e8e68dac6e37a11")
 (package! org-pretty-tags :pin "40fd72f3e701e31813f383fb429d30bb88cee769")
-;; (package! burly)
-;; (package! sublimity)
 (package! direnv)
-;; (package! kaolin-themes)
-;; (package! color-theme-sanityinc-tomorrow)
-;; (package! apropospriate-theme)
-;; (package! solarized-theme)
-;; (package! nano-emacs
-;;   :recipe (:host github :repo "rougier/nano-emacs"))
 (package! nov)
 (package! hide-lines)
 (package! org-krita
   :recipe (:host github
            :repo "lepisma/org-krita"
            :files ("resources" "resources" "*.el" "*.el")))
+
+;; (package! company-tabnine) ; tab9 autocomplete
+;; (package! kaolin-themes)
+;; (package! color-theme-sanityinc-tomorrow)
+;; (package! apropospriate-theme)
+;; (package! solarized-theme)
+;; (package! nano-emacs
+;;   :recipe (:host github :repo "rougier/nano-emacs"))
+;; (package! systemd :pin "51c148e09a")
+;; (package! org-super-agenda)
+;; (package! burly)
+;; (package! sublimity)
